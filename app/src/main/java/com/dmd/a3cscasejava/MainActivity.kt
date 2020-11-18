b536fc8ec20f16912e2e8b895abc8513412410b3
Mert <mert.demir@yenimedya.com.tr>
2020-11-18 20:13:53 +0300

    Initial Commit

---
+++ app/src/main/java/com/dmd/a3cscasejava/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.dmd.a3cscasejava
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dmd.a3cscasejava.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
